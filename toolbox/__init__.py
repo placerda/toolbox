4ead917cd9a685897d5bb270728a06a6323a371f
Paulo Lacerda <pclacerda@gmail.com>
2020-08-05 20:03:01 -0300

    initial

---
+++ toolbox/__init__.py
@@ -0,0 +1,7 @@
+# author:	Paulo Lacerda
+
+# set the version number
+__version__ = "0.0.1"
+
+# import the necessary packages
+from .convenience import dicom_to_jpeg
---
+++ toolbox/convenience.py
@@ -0,0 +1,12 @@
+import pydicom
+
+def dicom_to_jpeg(dicom_file):
+    """Converts DICOM to JPEG
+
+    Arguments:
+    dicom_file -- DICOM file path
+
+    Returns:
+    jpg_image -- ndarray of shape (None, 3), jpeg image
+    """
+    return 0
