ff8743b9d16d8adae40e6e1004da9da2f6de813a
Paulo Lacerda <pclacerda@gmail.com>
2020-08-05 20:26:53 -0300

    Initial versions.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='toolbox',
+    packages=['toolbox'],
+    version='0.0.1',
+    description='Some classes and functions I use frequently.',
+    author='Paulo Lacerda',
+    author_email='adrian@pyimagesearch.com',
+    url='https://github.com/placerda/toolbox',
+    download_url='https://github.com/placerda/toolbox/tarball/0.1',
+    keywords=['computer vision', 'image processing', 'opencv', 'matplotlib'],
+    classifiers=[],
+)
