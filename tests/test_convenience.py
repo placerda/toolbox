2a5bef46726e2f3788ce4ef5d45ec798d0053447
Paulo Lacerda <pclacerda@gmail.com>
2020-08-06 09:14:25 -0300

    Initial versions.

---
+++ tests/test_convenience.py
@@ -0,0 +1,23 @@
+import unittest
+import toolbox
+from matplotlib import pyplot as plt
+
+class TestToolbox(unittest.TestCase):
+    def test_dicom_to_jpeg(self):
+        filename = './tests/testdata/dummy.dcm'
+        result = toolbox.dicom_to_jpeg(filename)
+        plt.title('test_dicom_to_jpeg')
+        plt.imshow(result)
+        plt.show()
+        self.assertEqual(len(result.shape), 3)
+
+    def test_nifti_to_jpeg(self):
+        filename = './tests/testdata/dummy.nii.gz'
+        result = toolbox.nifti_to_jpeg(filename)
+        plt.title('test_nifti_to_jpeg')
+        plt.imshow(result)
+        plt.show()
+        self.assertEqual(len(result.shape), 3)
+
+if __name__ == '__main__':
+    unittest.main()
--- toolbox/convenience.py
+++ toolbox/convenience.py
@@ -1,12 +1,73 @@
 import pydicom
+import nibabel as nb
+import numpy as np
+import cv2
 
-def dicom_to_jpeg(dicom_file):
-    """Converts DICOM to JPEG
-
-    Arguments:
-    dicom_file -- DICOM file path
-
-    Returns:
-    jpg_image -- ndarray of shape (None, 3), jpeg image
+def quantize_hu_rgb(hu_data, window_length=-600, window_width=1500):
     """
-    return 0
+    Quantize HU in 256
+    :param hu_data: ndarray of shape (m, n, 1)
+    :param window_length: DICOM's window length, by default uses lung best window
+    :param window_width: DICOM's window width, by default uses lung best window
+    :return: ndarray of shape (m, n, 3) type uint8
+    """
+
+    # quantization parameters
+    WINDOW_LENGHT=window_length
+    WINDOW_WIDTH=window_width
+    MIN_HU=-1024
+    MAX_HU=1024
+
+    # 256 quantization
+    min_value = WINDOW_LENGHT - (WINDOW_WIDTH // 2)
+    if min_value < MIN_HU: min_value = MIN_HU
+    max_value = WINDOW_LENGHT + (WINDOW_WIDTH // 2)
+    if max_value > MAX_HU: max_value = MAX_HU
+    quantized = hu_data.copy()
+    quantized = np.clip(quantized, min_value, max_value)
+    for cell in np.nditer(quantized, op_flags=['readwrite']):
+        cell[...] = ((cell - min_value) * 255) // (max_value - min_value)
+    gray = quantized.astype('uint8')
+    image_rgb = cv2.cvtColor(gray, cv2.COLOR_GRAY2RGB)
+
+    return image_rgb
+
+def dicom_to_jpeg(dicom_file, window_length=-600, window_width=1500):
+    """
+    Reads DICOM file and returns it in JPEG equivalent format
+    :param dicom_file: DICOM file path
+    :param window_length: HU window length
+    :param window_width: HU window width
+    :return: ndarray of shape (n, n, 3)
+    """
+    # load dicom
+    ds = pydicom.dcmread(dicom_file)
+    b = ds.RescaleIntercept
+    m = ds.RescaleSlope
+    slice = m * ds.pixel_array + b
+
+    jpeg = quantize_hu_rgb(slice, window_length, window_width)
+
+    return jpeg
+
+def nifti_to_jpeg(nifti_file, window_length=-600, window_width=1500):
+    """
+    Reads NifTI file and returns middle slice in JPEG equivalent format
+    :param nifti_file: NifTI file path
+    :param window_length: HU window length, by default uses lung best window
+    :param window_width: HU window width, by default uses lung best window
+    :return: ndarray of shape (n, n, 3)
+    """
+    # load nifti
+    img = nb.load(nifti_file)
+    data = img.get_fdata()
+
+    # get middle slice
+    mean = data.shape[2] // 2
+    slice = data[:,:,mean]
+
+    # rotates nifti
+    slice = np.rot90(slice)
+    jpeg = quantize_hu_rgb(slice, window_length, window_width)
+
+    return jpeg
